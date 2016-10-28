fb7d95962b65bce7e6fe4abb1c9ab9386f942b94
Morgan <morgan.cordoba@hotmail.com>
2016-10-28 20:27:03 +0200

    Añadido la informacion del bugatti

--- app/src/main/java/com/example/morgan/appdecoches/MainActivity.java
+++ app/src/main/java/com/example/morgan/appdecoches/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.morgan.appdecoches;
 
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     //Declaro todas las variables de tipo ImageBotton con los respectivos nombres
-    ImageButton btnMustang, btnTesla,btnDodge,btnLexus,btnMorgan,btnCadillac,btnSmart,btnChevrolet,btnDodgeViper,btnBuagatti;
+    ImageButton btnBuagatti, btnCadillac, btnChevrolet, btnDodge, btnDodgeViper, btnLexus, btnMorgan, btnMustang, btnSmart, btnTesla;
+
+    //Creamos el bundle para meter dentro que boton han pulsado
+    Bundle b = new Bundle();
+
+    //Crea esta variable aqui para que sea accesible por varias metodos
+    String coche;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Hago el casting para relacionar todos los objetos con su view en la activity
-        btnBuagatti = (ImageButton) findViewById(R.id.imageButtonBuagatti);
+        btnBuagatti = (ImageButton) findViewById(R.id.imageButtonBugatti);
         btnCadillac = (ImageButton) findViewById(R.id.imageButtonCadillac);
         btnChevrolet = (ImageButton) findViewById(R.id.imageButtonChevrolet);
         btnDodge = (ImageButton) findViewById(R.id.imageButtonDodge);
@@ -29,6 +37,160 @@ public class MainActivity extends AppCompatActivity {
         btnTesla = (ImageButton) findViewById(R.id.imageButtonTesla);
 
 
+        /*
+        //Cremos los listener para cada boton para luego poder haver un switch
+        btnBuagatti.setOnClickListener(this);
+        btnCadillac.setOnClickListener(this);
+        btnChevrolet.setOnClickListener(this);
+        btnDodge.setOnClickListener(this);
+        btnDodgeViper.setOnClickListener(this);
+        btnLexus.setOnClickListener(this);
+        btnMorgan.setOnClickListener(this);
+        btnMustang.setOnClickListener(this);
+        btnSmart.setOnClickListener(this);
+        btnTesla.setOnClickListener(this);
+        */
+
     }
+
+    public void btnBugatti(View v)
+    {
+        //Creamos una vatiable para decir que coche han escogido y luego ponerlo en el bundle
+        coche = "bugatti";
+
+        //Metemos en un cajon del bundle un dato, le pones de id "coche"
+        //y metemos la variable que contiene que coche han elegido
+        b.putString("coche", coche.toString());
+
+        enviar(b);
+    }
+
+    public void btnCadillac(View v)
+    {
+        //Creamos una vatiable para decir que coche han escogido y luego ponerlo en el bundle
+        coche = "cadillac";
+
+        //Metemos en un cajon del bundle un dato, le pones de id "coche"
+        //y metemos la variable que contiene que coche han elegido
+        b.putString("coche", coche.toString());
+
+        enviar(b);
+    }
+
+
+    public void btnChevrolet(View v)
+    {
+        //Creamos una vatiable para decir que coche han escogido y luego ponerlo en el bundle
+        coche = "chevrolet";
+
+        //Metemos en un cajon del bundle un dato, le pones de id "coche"
+        //y metemos la variable que contiene que coche han elegido
+        b.putString("coche", coche.toString());
+
+        enviar(b);
+    }
+
+    public void btnDodge(View v)
+    {
+        //Creamos una vatiable para decir que coche han escogido y luego ponerlo en el bundle
+        coche = "dodge";
+
+        //Metemos en un cajon del bundle un dato, le pones de id "coche"
+        //y metemos la variable que contiene que coche han elegido
+        b.putString("coche", coche.toString());
+
+        enviar(b);
+    }
+
+    public void btnDodgeViper(View v)
+    {
+        //Creamos una vatiable para decir que coche han escogido y luego ponerlo en el bundle
+        coche = "dodgeViper";
+
+        //Metemos en un cajon del bundle un dato, le pones de id "coche"
+        //y metemos la variable que contiene que coche han elegido
+        b.putString("coche", coche.toString());
+
+        enviar(b);
+    }
+
+    public void btnLexus(View v)
+    {
+        //Creamos una vatiable para decir que coche han escogido y luego ponerlo en el bundle
+        coche = "lexus";
+
+        //Metemos en un cajon del bundle un dato, le pones de id "coche"
+        //y metemos la variable que contiene que coche han elegido
+        b.putString("coche", coche.toString());
+
+        enviar(b);
+    }
+
+    public void btnMorgan(View v)
+    {
+        //Creamos una vatiable para decir que coche han escogido y luego ponerlo en el bundle
+        coche = "morgan";
+
+        //Metemos en un cajon del bundle un dato, le pones de id "coche"
+        //y metemos la variable que contiene que coche han elegido
+        b.putString("coche", coche.toString());
+
+        enviar(b);
+    }
+
+    public void btnMustang(View v)
+    {
+        //Creamos una vatiable para decir que coche han escogido y luego ponerlo en el bundle
+        coche = "musang";
+
+        //Metemos en un cajon del bundle un dato, le pones de id "coche"
+        //y metemos la variable que contiene que coche han elegido
+        b.putString("coche", coche.toString());
+
+        enviar(b);
+    }
+
+    public void btnSmart(View v)
+    {
+        //Creamos una vatiable para decir que coche han escogido y luego ponerlo en el bundle
+        coche = "smart";
+
+        //Metemos en un cajon del bundle un dato, le pones de id "coche"
+        //y metemos la variable que contiene que coche han elegido
+        b.putString("coche", coche.toString());
+
+        enviar(b);
+    }
+
+    public void btnTesla(View v)
+    {
+        //Creamos una vatiable para decir que coche han escogido y luego ponerlo en el bundle
+        coche = "tesla";
+
+        //Metemos en un cajon del bundle un dato, le pones de id "coche"
+        //y metemos la variable que contiene que coche han elegido
+        b.putString("coche", coche.toString());
+
+        enviar(b);
+
+    }
+
+    public void enviar (Bundle b)
+    {
+        //Creamos un objeto del tipo Intent, que sirve para ir de una activity a otra
+        //diciendo primero donde estamos y despues de la coma, donde queremos ir
+        Intent intencion = new Intent(MainActivity.this, MainActivity2.class);
+
+        //Le añadimos al Intent que lleve con sigo el Bundle que lleva los datos que hemos guardado
+        intencion.putExtras(b);
+
+        //Activamos el Intent
+        startActivity(intencion);
+    }
+
+    /*public void onClick(View v)
+    {
+
+    }*/
 
 }
--- app/src/main/java/com/example/morgan/appdecoches/MainActivity2.java
+++ app/src/main/java/com/example/morgan/appdecoches/MainActivity2.java
@@ -1,7 +1,12 @@
 package com.example.morgan.appdecoches;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -9,5 +14,45 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //Creo los objetos de los tipos que hay en la activity
+        TextView textViewMarca = (TextView)findViewById(R.id.textViewMarca);
+        TextView textViewFundador = (TextView)findViewById(R.id.textViewFundador);
+        TextView textViewFundacion = (TextView)findViewById(R.id.textViewFundacion);
+        TextView textViewWeb = (TextView)findViewById(R.id.textViewWeb);
+        TextView textViewModelo = (TextView)findViewById(R.id.textViewModelo);
+        ImageView imageViewCoche = (ImageView)findViewById(R.id.imageViewCoche);
+
+        //Creamos un bundle y preguntamos si el Intent de la llamada lleva algo
+        //en el intent
+        Bundle b = getIntent().getExtras();
+
+        //Hacemos un switch para que saber que boton al pulsado y dependiendo
+        // de cual sea mostrar una imagen o otra
+        switch (b.getString("coche"))
+        {
+            case "bugatti":
+                textViewMarca.setText("Marca: Bugatti.");
+                textViewFundador.setText("Fundador: Ettore Bugatti.");
+                textViewFundacion.setText("Fundación: 1909.");
+
+                textViewWeb.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("Sitio web: http://www.bugatti.com/home/"));
+                        startActivity(i);
+                    }
+                });
+
+                textViewModelo.setText("Modelo: Veyron");
+                imageViewCoche.setImageResource(R.drawable.Bugatti_Veyron);
+
+                break;
+        }
+
+
+
+
+
     }
 }
